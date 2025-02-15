b406f71ba87518126148eef0110e4eb0d8b631ce
lssv6 <lucassousasilva@yahoo.com>
2025-02-15 13:45:04 -0300

    style: fixed tabs vs spaces

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,9 @@
 return {
-	"folke/which-key.nvim",
-	"nvim-tree/nvim-web-devicons", -- Dependency of lualine
-	"nvim-lualine/lualine.nvim",
+    "folke/which-key.nvim",
+    "nvim-lualine/lualine.nvim",
+    "nvim-tree/nvim-web-devicons", -- Dependency of lualine
 
-	-- Theme/Colorscheme 
+    -- Theme/Colorscheme 
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
 	-- lspconfig plugin. Manages LSP's
@@ -13,15 +13,15 @@ return {
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 
-
     -- Nvim completation engine
-	{"hrsh7th/nvim-cmp", config = function() require("config.nvim-cmp") end},
+    {"hrsh7th/nvim-cmp", config = function() require("config.nvim-cmp") end},
 
     -- Dependencies for nvim-cmp
-	"hrsh7th/cmp-nvim-lsp", -- lsp     auto-completion
-	"hrsh7th/cmp-buffer",   -- buffer  auto-completion
-	"hrsh7th/cmp-path",     -- path    auto-completion
-	"hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+    "hrsh7th/cmp-nvim-lsp", -- lsp     auto-completion
+    "hrsh7th/cmp-buffer",   -- buffer  auto-completion
+    "hrsh7th/cmp-path",     -- path    auto-completion
+    "hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+
     -- LuaSnip snippet engine. Also dependency for nvim-cmp
     {"L3MON4D3/LuaSnip",version = "v2.3.0", build = "make install_jsregexp"},
     -- Snippet engine is what writes the remaining
